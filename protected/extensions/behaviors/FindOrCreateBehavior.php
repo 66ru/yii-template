e702d307fdbea365dff099bfa215e4b2a074c3f0
m8rge <to.merge@gmail.com>
2012-09-01 20:42:15 +0600

    FindOrCreateBehavior.php

---
+++ protected/extensions/behaviors/FindOrCreateBehavior.php
@@ -0,0 +1,21 @@
+<?php
+
+class FindOrCreateBehavior extends CActiveRecordBehavior
+{
+	/**
+	 * @param mixed $value used for search
+	 * @param bool|string $fieldName false means primary pey field
+	 * @return CActiveRecord
+	 */
+	public function findOrCreate($value, $fieldName = false) {
+		if (!$fieldName)
+			$ar = $this->owner->findByPk($value);
+		else
+			$ar = $this->owner->findByAttributes(array($fieldName => $value));
+		$className = get_class($this->owner);
+		if (!$ar)
+			$ar = new $className;
+
+		return $ar;
+	}
+}
