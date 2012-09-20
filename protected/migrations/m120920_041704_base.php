33deac130ba0679411d62ba1352e4ecdf53e3c54
m8rge <to.merge@gmail.com>
2012-09-20 10:18:08 +0600

    add base migration

---
+++ protected/migrations/m120920_041704_base.php
@@ -0,0 +1,31 @@
+<?php
+
+class m120920_041704_base extends CDbMigration
+{
+	public function up()
+	{
+		$dumpFile = dirname(__FILE__).'/base.sql';
+
+		$db = Yii::app()->db;
+		$dbUser = $db->username;
+		$dbPass = $db->password;
+		if (preg_match('/host=([^;]*)/', $db->connectionString, $regs)) {
+			$dbHost = $regs[1];
+		} else {
+			throw new CException('can\'t get mysql host from connectionString');
+		}
+		if (preg_match('/dbname=([^;]*)/', $db->connectionString, $regs)) {
+			$dbName = $regs[1];
+		} else {
+			throw new CException('can\'t get mysql db name from connectionString');
+		}
+
+		system('mysql --host='.$dbHost.' --user=' . $dbUser . ' --password=' . $dbPass .' ' . $dbName . ' < '. realpath($dumpFile));
+	}
+
+	public function down()
+	{
+		echo "m120920_041704_base does not support migration down.\n";
+		return false;
+	}
+}
