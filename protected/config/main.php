070a28ca7cfbf0e84e4fdf7b216d4825a058b279
m8rge <to.merge@gmail.com>
2013-12-23 23:41:16 +0600

    introduce sentry

--- protected/config/main.php
+++ protected/config/main.php
@@ -4,11 +4,38 @@ Yii::setPathOfAlias('lib', realpath(__DIR__ . '/../../lib'));
 Yii::setPathOfAlias('vendor', realpath(__DIR__ . '/../../vendor'));
 
 $params = require('params.php');
+
+$components = array();
+$logRoutes = array(
+    array(
+        'class' => 'CFileLogRoute',
+        'levels' => 'error,warning',
+    ),
+    array(
+        'class' => 'CFileLogRoute',
+        'levels' => 'info',
+        'logFile' => 'info.log',
+    )
+);
+if ($params['useSentry']) {
+    $logRoutes[] = array(
+        'class'=>'vendor.m8rge.yii-sentry-log.RSentryLog',
+        'levels'=>'error, warning',
+        'except' => 'exception.*',
+        'dsn' => $params['sentryDSN'],
+    );
+    $components[] = array(
+        'RSentryException' => array(
+            'dsn' => $params['sentryDSN'],
+            'class' => 'vendor.m8rge.yii-sentry-log.RSentryComponent',
+        ),
+    );
+}
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => $params['appName'],
     'language' => 'ru',
-    'preload' => array('log'),
+    'preload' => array('log', 'RSentryException'),
     'import' => array(
         'application.models.*',
         'application.models.forms.*',
@@ -19,100 +46,94 @@ return array(
         'ext.mAdmin.*',
     ),
     'modules' => require(__DIR__.'/modules.php'),
-    'components' => array(
-        'user' => array(
-            // enable cookie-based authentication
-            'allowAutoLogin' => true,
-            'loginUrl' => array('site/login'),
-        ),
-        'request' => array(
-            'class' => 'application.components.SecuredCsrfHttpRequest',
-            'enableCsrfValidation' => true,
-            'noCsrfValidationRoutes'=>array(),
-        ),
-        'urlManager' => array(
-            'urlFormat' => 'path',
-            'urlSuffix' => '/',
-            'showScriptName' => false,
-            'rules' => array(
-                '/' => 'site/index',
-                'admin/' => 'system',
-                'admin/<module:\w+>/' => '<module>',
-                'admin/<module:\w+>/<controller:\w+>/' => '<module>/admin<controller>',
-                'admin/<module:\w+>/<controller:\w+>/<action:\w+>/' => '<module>/admin<controller>/<action>',
+    'components' => array_merge(
+        array(
+            'user' => array(
+                // enable cookie-based authentication
+                'allowAutoLogin' => true,
+                'loginUrl' => array('site/login'),
             ),
-        ),
-        'assetManager' => array(
-            'linkAssets' => true,
-        ),
-        'db' => array(
-            'connectionString' => 'mysql:host=' . $params['dbHost'] . ';dbname=' . $params['dbName'],
-            'emulatePrepare' => true,
-            'username' => $params['dbLogin'],
-            'password' => $params['dbPassword'],
-            'charset' => 'utf8',
-        ),
-        'authManager' => array(
-            'class' => 'CDbAuthManager',
-            'connectionID' => 'db',
-        ),
-        'fs' => array(
-            'class' => 'FileSystem',
-            'nestedFolders' => 1,
-        ),
-        'format' => array(
-            'booleanFormat' => array(
-                'Нет',
-                'Да',
+            'request' => array(
+                'class' => 'application.components.SecuredCsrfHttpRequest',
+                'enableCsrfValidation' => true,
+                'noCsrfValidationRoutes'=>array(),
             ),
-        ),
-        'viewRenderer' => array(
-            'class' => 'vendor.yiiext.twig-renderer.ETwigViewRenderer',
-            'twigPathAlias' => 'vendor.twig.twig.lib.Twig',
-            'options' => array(
-                'autoescape' => true,
-            ),
-            'functions' => array(
-                'widget' => array(
-                    0 => 'TwigFunctions::widget',
-                    1 => array('is_safe' => array('html')),
+            'urlManager' => array(
+                'urlFormat' => 'path',
+                'urlSuffix' => '/',
+                'showScriptName' => false,
+                'rules' => array(
+                    '/' => 'site/index',
+                    'admin/' => 'system',
+                    'admin/<module:\w+>/' => '<module>',
+                    'admin/<module:\w+>/<controller:\w+>/' => '<module>/admin<controller>',
+                    'admin/<module:\w+>/<controller:\w+>/<action:\w+>/' => '<module>/admin<controller>/<action>',
                 ),
-                'const' => 'TwigFunctions::constGet',
-                'static' => 'TwigFunctions::staticCall',
-                'url' => 'TwigFunctions::url',
-                'absUrl' => 'TwigFunctions::absUrl',
-                'plural' => 'TwigFunctions::plural',
             ),
-            'filters' => array(
-                'unset' => 'TwigFunctions::_unset',
+            'assetManager' => array(
+                'linkAssets' => true,
             ),
-        ),
-        'bootstrap' => array(
-            'class' => 'vendor.clevertech.yii-booster.src.components.Bootstrap',
-            'responsiveCss' => true,
-            'jqueryCss' => false,
-            'minify' => !YII_DEBUG,
-        ),
-        'errorHandler' => array(
-            'errorAction' => 'site/error',
-        ),
-        'image' => array(
-            'class' => 'ext.image.CImageComponent',
-            'driver' => $params['imageDriver'],
-        ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning',
+            'db' => array(
+                'connectionString' => 'mysql:host=' . $params['dbHost'] . ';dbname=' . $params['dbName'],
+                'emulatePrepare' => true,
+                'username' => $params['dbLogin'],
+                'password' => $params['dbPassword'],
+                'charset' => 'utf8',
+            ),
+            'authManager' => array(
+                'class' => 'CDbAuthManager',
+                'connectionID' => 'db',
+            ),
+            'fs' => array(
+                'class' => 'FileSystem',
+                'nestedFolders' => 1,
+            ),
+            'format' => array(
+                'booleanFormat' => array(
+                    'Нет',
+                    'Да',
                 ),
-                // uncomment the following to show log messages on web pages
-                /*array(
-                    'class'=>'CWebLogRoute',
-                ),*/
+            ),
+            'viewRenderer' => array(
+                'class' => 'vendor.yiiext.twig-renderer.ETwigViewRenderer',
+                'twigPathAlias' => 'vendor.twig.twig.lib.Twig',
+                'options' => array(
+                    'autoescape' => true,
+                ),
+                'functions' => array(
+                    'widget' => array(
+                        0 => 'TwigFunctions::widget',
+                        1 => array('is_safe' => array('html')),
+                    ),
+                    'const' => 'TwigFunctions::constGet',
+                    'static' => 'TwigFunctions::staticCall',
+                    'url' => 'TwigFunctions::url',
+                    'absUrl' => 'TwigFunctions::absUrl',
+                    'plural' => 'TwigFunctions::plural',
+                ),
+                'filters' => array(
+                    'unset' => 'TwigFunctions::_unset',
+                ),
+            ),
+            'bootstrap' => array(
+                'class' => 'vendor.clevertech.yii-booster.src.components.Bootstrap',
+                'responsiveCss' => true,
+                'jqueryCss' => false,
+                'minify' => !YII_DEBUG,
+            ),
+            'errorHandler' => array(
+                'errorAction' => 'site/error',
+            ),
+            'image' => array(
+                'class' => 'ext.image.CImageComponent',
+                'driver' => $params['imageDriver'],
+            ),
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => $logRoutes,
             ),
         ),
+        $components
     ),
     'params' => $params,
 );
--- protected/config/params-dist.php
+++ protected/config/params-dist.php
@@ -3,11 +3,14 @@
 return array(
     'appName' => '',
     'yiiDebug' => true,
+    'useSentry' => true,
 
     'dbHost' => '127.0.0.1',
     'dbName' => '',
     'dbLogin' => '',
     'dbPassword' => '',
+
+    'sentryDSN' => '',
 
     'imageDriver' => '', //Image_GD_Driver,Image_ImageMagick_Driver
 );
