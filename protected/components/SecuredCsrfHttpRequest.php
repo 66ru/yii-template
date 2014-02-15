61db4176aeefb09b2db572df7d6205746e5c012f
m8rge <to.merge@gmail.com>
2014-02-15 13:24:42 +0600

    SecuredCsrfHttpRequest.php improve

--- protected/components/SecuredCsrfHttpRequest.php
+++ protected/components/SecuredCsrfHttpRequest.php
@@ -10,12 +10,14 @@ class SecuredCsrfHttpRequest extends CHttpRequest
         parent::normalizeRequest();
         //remove the event handler CSRF if this is a route we want skipped
         if ($this->enableCsrfValidation) {
-            $url = Yii::app()->getUrlManager()->parseUrl($this);
-            foreach ($this->noCsrfValidationRoutes as $route) {
-                if (strpos($url, $route) === 0) {
-                    Yii::app()->detachEventHandler('onBeginRequest', array($this, 'validateCsrfToken'));
+            try {
+                $url = Yii::app()->getUrlManager()->parseUrl($this);
+                foreach ($this->noCsrfValidationRoutes as $route) {
+                    if (strpos($url, $route) === 0) {
+                        Yii::app()->detachEventHandler('onBeginRequest', array($this, 'validateCsrfToken'));
+                    }
                 }
-            }
+            } catch (CHttpException $e) {}
         }
     }
 
