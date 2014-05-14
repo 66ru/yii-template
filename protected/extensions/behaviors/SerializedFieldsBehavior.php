ab26e16a6f209024bf2248ed1c63ae9aac2bcd78
m8rge <to.merge@gmail.com>
2014-05-14 12:58:05 +0600

    improve

--- protected/extensions/behaviors/SerializedFieldsBehavior.php
+++ protected/extensions/behaviors/SerializedFieldsBehavior.php
@@ -5,14 +5,14 @@ class SerializedFieldsBehavior extends CActiveRecordBehavior
     /**
      * @var string[]|string Array or comma separated string with field names that should be serialized in db.
      */
-    public $serializedFields = array();
+    public $fields = array();
 
     private function getSerializedFields()
     {
-        if (is_array($this->serializedFields)) {
-            return $this->serializedFields;
+        if (is_array($this->fields)) {
+            return $this->fields;
         } else {
-            $res = explode(',', $this->serializedFields);
+            $res = explode(',', $this->fields);
             array_walk(
                 $res,
                 function (&$value) {
@@ -28,6 +28,15 @@ class SerializedFieldsBehavior extends CActiveRecordBehavior
     {
         foreach ($this->getSerializedFields() as $fieldName) {
             $this->getOwner()->$fieldName = json_encode($this->getOwner()->$fieldName);
+        }
+    }
+
+    public function afterSave($event)
+    {
+        foreach ($this->getSerializedFields() as $fieldName) {
+            if (is_string($this->getOwner()->$fieldName)) {
+                $this->getOwner()->$fieldName = json_decode($this->getOwner()->$fieldName, true);
+            }
         }
     }
 
